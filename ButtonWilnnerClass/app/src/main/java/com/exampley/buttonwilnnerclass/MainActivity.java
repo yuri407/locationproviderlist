61780d1329cdd9b9114c14f0c3538cc592648b72
yuri <dbfl4387@naver.com>
2021-03-30 11:34:29 +0900

    added 2nd button with new class

--- ButtonWilnnerClass/app/src/main/java/com/exampley/buttonwilnnerclass/MainActivity.java
+++ ButtonWilnnerClass/app/src/main/java/com/exampley/buttonwilnnerclass/MainActivity.java
@@ -3,11 +3,12 @@ package com.exampley.buttonwilnnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button mButton1;
+    Button mButton1, mButton2;
     TextView mTextView1;
 
 
@@ -17,8 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener(this));
+        mButton1.setOnClickListener(new MyOnClickListener());
+        mButton2.setOnClickListener(new MyOnClickListener2());
+    }
+
+    class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            mTextView1.setText("You clicked YR's button");
+        }
+    }
+
+    private class MyOnClickListener2 implements View.OnClickListener {
+        public void onClick(View view) {
+            mTextView1.setText("You clicked YR's 2nd button");
+        }
+    }
     }
 }
+
+
