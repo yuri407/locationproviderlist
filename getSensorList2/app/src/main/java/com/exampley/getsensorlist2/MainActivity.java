3aab914d746d7649951e087124ee765fa764b4cd
yuri <dbfl4387@naver.com>
2021-04-06 10:41:27 +0900

    Sensor List

---
+++ getSensorList2/app/src/main/java/com/exampley/getsensorlist2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.exampley.getsensorlist2;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTxtSensors;
+    SensorManager sensorMgr;
+    List<Sensor> sensorList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTxtSensors = findViewById(R.id.txtSensors);
+        mTxtSensors.setMovementMethod(new ScrollingMovementMethod());
+
+        sensorMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+        mTxtSensors.append("(# Sensor: " + sensorList.size()+")\n\n");
+        for(Sensor sensor : sensorList){
+            mTxtSensors.append("Sensor name :"+sensor.getName() + "\n");
+            mTxtSensors.append("Sensor type :"+sensor.getType() + "\n\n");
+        }
+
+
+    }
+}
