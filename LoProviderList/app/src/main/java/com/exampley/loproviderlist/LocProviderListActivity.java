a82797740e17b955c26b663801bc0b42c37e0ae8
yuri <dbfl4387@naver.com>
2021-04-07 14:54:16 +0900

    list location providers

---
+++ LoProviderList/app/src/main/java/com/exampley/loproviderlist/LocProviderListActivity.java
@@ -0,0 +1,33 @@
+package com.exampley.loproviderlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class LocProviderListActivity extends AppCompatActivity {
+    TextView mTextView;
+    LocationManager locMgr;
+    List<String> locProviders;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.txtOutput);
+        locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locProviders = locMgr.getAllProviders();
+
+        String s = "";
+        for(int i = 0; i < locProviders.size(); i++){
+            s += "Loc. Provider " + locProviders.get(i) + "\n"
+                    + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
+        }
+        mTextView.setText(s);
+    }
+}
